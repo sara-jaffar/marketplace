33c16ec92504274924482cb4a2871580260da351
sara-jaffar <sara.alshoula55@email.com>
2025-07-19 22:05:13 +0300

    Finished the marketplace

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,49 +1,54 @@
-const express = require('express');
-const router = express.Router();
-const Listing = require('../models/listing');
-const isSignedIn = require('../middleware/is-signed-in');
+const express = require('express')
+const router = express.Router()
+const Listing = require('../models/listing')
+const isSignedIn = require('../middleware/is-signed-in')
+const upload = require('../config/multer')
+
 
 // VIEW NEW LISTING FORM
-router.get('/new', (req, res) => {
-    res.render('listings/news.ejs')
+router.get('/new', isSignedIn, (req, res) => {
+    res.render('listings/new.ejs')
 })
 
-// POST FORM DATA TO THE DATABASE
-router.post('/', async (req, res) => {
+// POST FORM DATA TO DATABASE
+router.post('/', isSignedIn, upload.single('image'), async (req, res) => {
     try {
-        req.body.seller = req.session.user._id;
-        await Listing.create(req.body);
+        req.body.seller = req.session.user._id
+        req.body.image = {
+            url: req.file.path,
+            cloudinary_id: req.file.filename
+        }
+        await Listing.create(req.body)
         res.redirect('/listings')
-    } catch(error) {
-        console.log(error);
-        res.send('something went wrong');
+    } catch (error) {
+        console.log(error)
+        res.send('Something went wrong')
     }
 })
 
-// VIEW INDEX PAGE
+// VIEW THE INDEX PAGE
 router.get('/', async (req, res) => {
-   const foundListings =  await Listing.find();
-   console.log(foundListings);
-   res.render('listings/index.ejs', { foundListings: foundListings});
+    const foundListings = await Listing.find()
+    console.log(foundListings)
+    res.render('listings/index.ejs', { foundListings: foundListings })
 })
 
 // VIEW A SINGLE LISTING (SHOW PAGE)
 router.get('/:listingId', async (req, res) => {
     try {
-        const foundListing = await Listing.findById(req.params.listingId).populate('seller').populate('comments.author');
-        console.log(foundListing);
-        console.log(req.session.user);
-        res.render('listings/show.ejs', { foundListing: foundListing });
-    } catch(error) {
-        console.log(error);
-        res.redirect('/');
+        const foundListing = await Listing.findById(req.params.listingId).populate('seller').populate('comments.author')
+        console.log(foundListing)
+        res.render('listings/show.ejs', { foundListing: foundListing })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
     }
 })
 
-// DELETE LISTING FROMM THE DATABASE
-router.delete('/:listingId', async (req, res) => {
+// DELETE LISTING FROM DATABASE
+router.delete('/:listingId', isSignedIn, async (req, res) => {
     // find the listing
-    const foundListing = await Listing.findById(req.params.listingId).populate('seller');
+    const foundListing = await Listing.findById(req.params.listingId).populate('seller')
     // check if the logged in user owns the listing
     if (foundListing.seller._id.equals(req.session.user._id)) {
         // delete the listing and redirect
@@ -53,23 +58,26 @@ router.delete('/:listingId', async (req, res) => {
     return res.send('Not authorized')
 })
 
-//RENDER THE EDIT FORM VIEW
-router.get('/:listingId/edit', async (req, res) => {
-    const foundListing = await Listing.findById(req.params.listingId).populate('seller');
+// RENDER THE EDIT FORM VIEW
+router.get('/:listingId/edit', isSignedIn, async (req, res) => {
+    const foundListing = await Listing.findById(req.params.listingId).populate('seller')
+
     if (foundListing.seller._id.equals(req.session.user._id)) {
-        return res.render('listings/edit.ejs', { foundListing: foundListing});
+        return res.render('listings/edit.ejs', { foundListing: foundListing} )
     } 
-    return res.send('Not authorized');
+        return res.send('Not authorized')
+   
+
 })
 
-router.put('/:listingId', async (req, res) => {
-    const foundListing = await Listing.findById(req.params.listingId).populate('seller');
-
+router.put('/:listingId', isSignedIn, async (req, res) => {
+    const foundListing = await Listing.findById(req.params.listingId).populate('seller')
     if (foundListing.seller._id.equals(req.session.user._id)) {
-        await Listing.findByIdAndUpdate(req.params.listingId, req.body, { new: true });
-        return res.redirect(`/listings/${req.params.listingId}`);
+        await Listing.findByIdAndUpdate(req.params.listingId, req.body, { new: true })
+        return res.redirect(`/listings/${req.params.listingId}`)
     } 
-    return res.send('Not authorized');
+        return res.send('Not authorized')
+   
 })
 
 // POST COMMENT FORM TO THE DATABASE
@@ -82,4 +90,4 @@ router.post('/:listingId/comments', isSignedIn, async (req, res) => {
     res.redirect(`/listings/${req.params.listingId}`)
 })
 
-module.exports = router;
+module.exports = router
--- models/listing.js
+++ models/listing.js
@@ -1,23 +1,27 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const commentSchema = new mongoose.Schema({
     content: String,
-    author:  { 
+    author: { 
         type: Schema.Types.ObjectId,
         ref: 'User'
     }
 }, { timestamps: true })
+
 const listingSchema = new mongoose.Schema({
     title: String,
     description: String,
-    price: Number,
-    image: String,
+    price: Number, 
+    image: {
+        url: { type: String, required: true },
+        cloudinary_id: { type: String, required: true}
+    },
     seller: { 
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
-    comments: [commentSchema],
-}, { timeseries: true })
+    comments: [commentSchema]
+}, { timestamps: true })
 
-module.exports = mongoose.model('Listing', listingSchema);
+module.exports = mongoose.model('Listing', listingSchema)
